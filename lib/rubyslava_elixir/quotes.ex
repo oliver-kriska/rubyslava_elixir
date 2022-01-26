0d9223fa480511f1b63b66febe0ad458585de8f0
Oliver Kriska <oliver@ideax.sk>
2022-01-26 22:13:44 +0100

    Quotes - smaller broadcast period

--- lib/rubyslava_elixir/quotes.ex
+++ lib/rubyslava_elixir/quotes.ex
@@ -14,7 +14,7 @@ defmodule RubyslavaElixir.Quotes do
   # @quotes_source_url "https://raw.githubusercontent.com/akhiltak/inspirational-quotes/master/Quotes.csv"
   @quotes_source_url "https://gist.githubusercontent.com/JakubPetriska/060958fd744ca34f099e947cd080b540/raw/963b5a9355f04741239407320ac973a6096cd7b6/quotes.csv"
   @pub_sub_server RubyslavaElixir.PubSub
-  @broadcast_period 15
+  @broadcast_period 5
 
   @impl true
   def init(_) do
